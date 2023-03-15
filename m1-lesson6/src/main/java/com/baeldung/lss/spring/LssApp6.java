72e1fdcde7e5e0cdfc32e268efb9aa615fc763b2
birddevelper <mostafa_shaeri_tj@yahoo.com>
2023-03-15 01:38:28 -0700

    • Change ComponentScan argument to "com.baeldung.lss" • Remove UserRepository bean

--- m1-lesson6/src/main/java/com/baeldung/lss/spring/LssApp6.java
+++ m1-lesson6/src/main/java/com/baeldung/lss/spring/LssApp6.java
@@ -2,22 +2,14 @@ package com.baeldung.lss.spring;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.ComponentScan;
 
-import com.baeldung.lss.persistence.InMemoryUserRepository;
-import com.baeldung.lss.persistence.UserRepository;
 
 @SpringBootApplication
-@ComponentScan("com.baeldung.lss.web")
+@ComponentScan("com.baeldung.lss")
 public class LssApp6 { 
-    
-    @Bean
-    public UserRepository userRepository() {
-        return new InMemoryUserRepository();
-    }
 
-    public static void main(String[] args) throws Exception {
+    public static void main(String[] args) {
         SpringApplication.run(new Class[] { LssApp6.class, LssSecurityConfig.class, LssWebMvcConfiguration.class }, args);
     }
 
